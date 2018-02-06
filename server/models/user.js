91a83cbb679f0b8a25e93e821633496a413c8912
Eric <leftclick.coding123@gmail.com>
2018-02-06 18:22:27 +0800

    Add test for GET /users/me and POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -72,7 +72,7 @@ UserSchema.statics.findByToken = function(token) {
 UserSchema.pre('save', function(next) {
     const user = this;
 
-    if (!user.isModified('password')) {
+    if (user.isModified('password')) {
         
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
